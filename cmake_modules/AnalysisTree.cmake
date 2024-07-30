9e03074494479ec00be73c5298ddfcbfab6824a3
Oleksii Lubynets <66421517+lubynets@users.noreply.github.com>
2024-07-30 14:17:14 +0200

    improve auto-install of AT; improve config.sh (#26)

--- cmake_modules/AnalysisTree.cmake
+++ cmake_modules/AnalysisTree.cmake
@@ -1,17 +1,26 @@
-if (AnalysisTreeQA_BUNDLED_AT)
+if(AnalysisTreeQA_BUNDLED_AT)
     message("-- Building bundled AnalysisTree")
     include(FetchContent)
 
     FetchContent_Declare(
             AnalysisTree
-            GIT_REPOSITORY "https://github.com/HeavyIonAnalysis/AnalysisTree.git"
+            GIT_REPOSITORY ${AnalysisTreeQA_BUNDLED_AT_URL}
             GIT_TAG ${AnalysisTreeQA_BUNDLED_AT_VERSION}
             GIT_SHALLOW ${AnalysisTreeQA_BUNDLED_AT_GIT_SHALLOW}
     )
     FetchContent_MakeAvailable(AnalysisTree)
+    list(APPEND PROJECT_INCLUDE_DIRECTORIES ${AnalysisTree_BINARY_DIR}/include)
+    list(APPEND PROJECT_LINK_LIBRARIES AnalysisTreeBase AnalysisTreeInfra)
+    message(STATUS "AT: ${AnalysisTree_BINARY_DIR} ${AnalysisTree_SOURCE_DIR}")
 else()
-    list(APPEND CMAKE_PREFIX_PATH ${ANALYSISTREE_HOME})
-    list(APPEND CMAKE_PREFIX_PATH $ENV{ANALYSISTREE_HOME})
-    find_package(AnalysisTree REQUIRED)
-    list(APPEND PROJECT_INCLUDE_DIRECTORIES ${AnalysisTree_INCLUDE_DIR})
+    find_package(AnalysisTree QUIET)
+    if(AnalysisTree_FOUND)
+        list(APPEND CMAKE_PREFIX_PATH ${ANALYSISTREE_HOME})
+        list(APPEND CMAKE_PREFIX_PATH $ENV{ANALYSISTREE_HOME})
+        list(APPEND PROJECT_INCLUDE_DIRECTORIES ${AnalysisTree_INCLUDE_DIR})
+        list(APPEND PROJECT_LINK_LIBRARIES AnalysisTreeBase AnalysisTreeInfra)
+    else()
+        message(FATAL_ERROR "Set AnalysisTree_DIR to a directory containing AnalysisTreeConfig.cmake file\n"
+                            "OR\nuse -DAnalysisTreeQA_BUNDLED_AT=ON option to build AnalysisTree automatically")
+    endif()
 endif()
